35b09c7b7d5e7c775bbf088848c683d0836b682f
katarinasandart <katarina.sandart@hotmail.com>
2018-08-22 14:13:42 +0200

    Add popup window for checkout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,31 +2,18 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-import Winelist from "./Wine/WineList";
-
 import Navigation from "./Components/Navigation";
 import ProductList from "./Components/ProductList";
-import data from "./data";
-import deepstream from "deepstream.io-client-js";
+import ExampleApp from "./Components/ExampleApp";
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: data,
+      products: [],
       cart: []
     };
-
-    this.handleAddToCart = this.handleAddToCart.bind(this);
-  }
-
-  handleAddToCart(product) {
-    const cartItem = this.state.cart.find(x => x.id === product.id);
-    product.inStock > 0 && this.setState({ cart: [...this.state.cart, product] });
-    this.state.products.forEach(e => {if (e===product && e.inStock>0) e.inStock--})}
-  
-
-  componentDidMount() {
     axios
       .get("http://localhost:8090")
       .then(response => {
@@ -41,22 +28,35 @@ class App extends Component {
         this.setState(newState);
       })
       .catch(error => console.log(error));
+      this.handleAddToCart = this.handleAddToCart.bind(this);
   }
-  //<Winelist wine={this.state.wine} />
+
+  handleAddToCart(product) {
+    const cartItem = this.state.cart.find(x => x.id === product.id);
+    product.prisinklmoms > 0 && this.setState({ cart: [...this.state.cart, product] });
+    this.state.products.forEach(e => {if (e===product && e.prisinklmoms>0) e.prisinklmoms--})
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Vinshoppen</h1>
+          <Navigation cart={this.state.cart} />
+          <ExampleApp/>
         </header>
+        <div id="wrapper">
+        <div className="sidebar">
 
-        <Navigation cart={this.state.cart} />
+        </div>
+        
         <div className="App-main">
           <ProductList
             products={this.state.products}
             handleAddToCart={this.handleAddToCart}
           />
+        </div>
         </div>
       </div>
     );
