4ae3c29ac8c8de09c925907d630c209ac5b133cd
wallners <wallnerstrom@gmail.com>
2018-08-22 14:34:39 +0200

    add new header and sidebar structure

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-import Winelist from "./Wine/WineList";
 
 import Navigation from "./Components/Navigation";
 import ProductList from "./Components/ProductList";
 import data from "./data";
-import deepstream from "deepstream.io-client-js";
 
 class App extends Component {
   constructor(props) {
@@ -22,9 +20,13 @@ class App extends Component {
 
   handleAddToCart(product) {
     const cartItem = this.state.cart.find(x => x.id === product.id);
-    product.inStock > 0 && this.setState({ cart: [...this.state.cart, product] });
-    this.state.products.forEach(e => {if (e===product && e.inStock>0) e.inStock--})}
-  
+    product.inStock > 0 &&
+      this.setState({ cart: [...this.state.cart, product] });
+    this.state.products.forEach(e => {
+      if (e === product && e.inStock > 0) e.inStock--;
+    });
+  }
+
   state = {
     wine: []
   };
@@ -50,17 +52,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Vinshoppen</h1>
-        </header>
+        <div className="wrapper">
+          <div id="left" class="column">
+            <div class="top-left">
+              <div className="set-height" />
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div class="bottom">
+              <a href="#white">Vitt</a> <br/>
+              <a href="#red">Rött</a>   <br/>
+              <a href="#sparkling">Mousserande</a>  <br/>
+            </div>
+          </div>
 
-        <Navigation cart={this.state.cart} />
-        <div className="App-main">
-          <ProductList
-            products={this.state.products}
-            handleAddToCart={this.handleAddToCart}
-          />
+          <div id="middle" class="column">
+            <div class="top-middle">
+              {" "}
+              <div className="set-height" />
+            </div>
+
+            <div class="bottom">
+              <ProductList
+                products={this.state.products}
+                handleAddToCart={this.handleAddToCart}
+              />
+            </div>
+          </div>
+          <div id="right" class="column">
+            <div class="top-right">
+            
+
+              <div className="set-height">
+              <Navigation cart={this.state.cart} /></div>
+            </div>
+            <div class="bottom">
+            <div className="set-width" />
+
+            </div>
+          </div>
         </div>
       </div>
     );
