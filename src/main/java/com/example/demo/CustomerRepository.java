786fe62ef78c58a20820f8bc94c0bf124c22ca73
Gabriela <g.saavedra.letelier@gmail.com>
2018-08-23 14:35:12 +0200

    fix

--- src/main/java/com/example/demo/CustomerRepository.java
+++ src/main/java/com/example/demo/CustomerRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface CustomerRepository extends JpaRepository{
 }
