ac5de6006a1aa8ae1bf90e616a6bc0631528747f
awildt <anna.wildt@hotmail.com>
2018-08-23 09:43:01 +0200

    changes

---
+++ react-app/src/Components/Filter.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Button from 'react-button-component';
+
+class Filter extends Component {
+    constructor(props) {
+        super(props)
+        this 
+      };
+
+      render() {
+          return (
+        <Button onclick={() => filterRedWine()}> 
+            Rött vin
+        </Button>
+          )};
+
+      filterRedWine() {
+
+      }
+
+    }
+    export default Filter;
