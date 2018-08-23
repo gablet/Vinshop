53aec8239761fff50358eca1d7dd7379ea72ff44
Johan <aronsson.86@gmail.com>
2018-08-23 14:30:05 +0200

    imlpemented save to db order

--- src/main/java/com/example/demo/OrderController.java
+++ src/main/java/com/example/demo/OrderController.java
@@ -1,15 +1,14 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+
 
 @RestController
 public class OrderController {
@@ -20,13 +19,13 @@ public class OrderController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping(value = "/createorder", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void createOrder(@RequestBody Customer customer, HttpServletResponse response) {
+    public void createOrder(@RequestBody List<Order> order, HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", "*");
 
+        repository.save(order);
+
         //repository.save(order);
-
-
-        System.out.println("Helllo " + customer.getFirstname());
+        //System.out.println(order.getArtikelnr());
     }
 
 
