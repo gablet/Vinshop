0a743cb7ef08a8ebde3b7353e03f23fadac7d0aa
katarinasandart <katarina.sandart@hotmail.com>
2018-08-23 11:01:23 +0200

    Add postmethod in react

--- react-app/src/Components/Popup.js
+++ react-app/src/Components/Popup.js
@@ -3,13 +3,15 @@ import ReactModal from 'react-modal';
 import Axios from 'axios';
 
 
+
+
 class Popup extends React.Component {
   constructor () {
     super();
     this.state = {
       showModal: false,
     };
-    
+  
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,17 +26,18 @@ class Popup extends React.Component {
   }
 
   handleSubmit(event) {
+    event.preventDefault();
     const form = event.target;
     const data = new FormData(form);
-    for (let name of data.keys()) {
-      form.elements[name];
+    for (var p of data) {
+      console.log(p.value);
     }
-    fetch('http://localhost:8080/createorder', {
-      method: 'POST',
-      body: data,
-    });
+    console.log(form);
     console.log(data);
-    event.preventDefault();
+    const url = 'http://localhost:8090/createorder';
+    Axios.post(url, {
+      data,
+    });
   }
 
   
@@ -49,14 +52,22 @@ class Popup extends React.Component {
         >
           <h1>Leveransinformation: </h1>
           <form className="form" onSubmit={this.handleSubmit}>
-            <label>Förnamn: <input type="text" id="firstname" name="firstname" /></label>
-            <label>Efternamn: <input type="text" id="lastname" name="lastname"  /></label>
-            <label>Email: <input type="text" id="email" name="email" /></label>
-            <label>Adress: <input type="text" id="adress" name="adress" /></label>
-            <label>Adress 2: <input type="text" id="adress2" name="adress2"  /></label>
-            <label>Postnummer: <input type="text" id="zipcode" name="zipcode" /></label>
-            <label>Postort: <input type="text" id="city" name="city" /></label>
-            <label>Land: <input type="text" id="country" name="country" /></label>
+            <label>Förnamn: 
+              <input type="text" id="firstname" name="firstname" /></label>
+            <label>Efternamn: 
+              <input type="text" id="lastname" name="lastname"  /></label>
+            <label>Email: 
+              <input type="text" id="email" name="email" /></label>
+            <label>Adress: 
+              <input type="text" id="adress" name="adress" /></label>
+            <label>Adress 2: 
+              <input type="text" id="adress2" name="adress2"  /></label>
+            <label>Postnummer: 
+              <input type="text" id="zipcode" name="zipcode" /></label>
+            <label>Postort: 
+              <input type="text" id="city" name="city" /></label>
+            <label>Land: 
+              <input type="text" id="country" name="country" /></label>
             <input type="submit" value="Submit" />
           </form>
           <button onClick={this.handleCloseModal}>Avbryt köp</button>
